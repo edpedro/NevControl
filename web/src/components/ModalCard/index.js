3211497f1f9b44e6697b836e7c66b4e6fa333971
Eduardo <edp2013.ep@gmail.com>
2021-04-08 20:43:35 -0300

    refactor: Ajuste no modal no Card

--- web/src/components/ModalCard/index.js
+++ web/src/components/ModalCard/index.js
@@ -107,28 +107,92 @@ function ModalCard({ isOpen, onChange, id }) {
                 </GridLimit>
                 <GridDate>
                   <div>
-                    <Label>Fechar dia</Label>
-                    <Input
-                      type="month"
-                      name="close"
-                      width={240}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.close}
-                    />
-                    <Span>{errors.close}</Span>
+                    <Label>
+                      Fechar dia
+                      <Select
+                        name="close"
+                        onChange={handleChange}
+                        value={values.close}
+                      >
+                        <option value="">Selecione</option>
+                        <option value="01">01</option>
+                        <option value="02">02</option>
+                        <option value="03">03</option>
+                        <option value="04">04</option>
+                        <option value="05">05</option>
+                        <option value="06">06</option>
+                        <option value="07">07</option>
+                        <option value="08">08</option>
+                        <option value="09">09</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                        <option value="26">26</option>
+                        <option value="27">27</option>
+                        <option value="28">28</option>
+                        <option value="29">29</option>
+                        <option value="30">30</option>
+                        <option value="31">31</option>
+                      </Select>
+                      <Span>{errors.close}</Span>
+                    </Label>
                   </div>
                   <div>
-                    <Label>Vence dia</Label>
-                    <Input
-                      type="month"
-                      name="win"
-                      width={240}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.win}
-                    />
-                    <Span>{errors.win}</Span>
+                    <Label>
+                      Vence dia
+                      <Select
+                        name="win"
+                        onChange={handleChange}
+                        value={values.win}
+                      >
+                        <option value="">Selecione</option>
+                        <option value="01">01</option>
+                        <option value="02">02</option>
+                        <option value="03">03</option>
+                        <option value="04">04</option>
+                        <option value="05">05</option>
+                        <option value="06">06</option>
+                        <option value="07">07</option>
+                        <option value="08">08</option>
+                        <option value="09">09</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                        <option value="26">26</option>
+                        <option value="27">27</option>
+                        <option value="28">28</option>
+                        <option value="29">29</option>
+                        <option value="30">30</option>
+                        <option value="31">31</option>
+                      </Select>
+                      <Span>{errors.win}</Span>
+                    </Label>
                   </div>
                 </GridDate>
                 <GridInstitution>
--- web/src/components/ModalCard/schema.js
+++ web/src/components/ModalCard/schema.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 export default Yup.object().shape({
   name: Yup.string().required('Nome é obrigatório'),
   limit: Yup.number().required('Limite é obrigatório'),
-  close: Yup.date().required('Fechar dia é obrigatório'),
-  win: Yup.date().required('Vencer dia obrigatório!'),
+  close: Yup.string().required('Fechar dia é obrigatório'),
+  win: Yup.string().required('Vencer dia obrigatório!'),
   bank: Yup.string().required('Bandeira obrigatório!'),
 });
